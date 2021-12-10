f245cc2a592c2a56aea5ab843ad834defda35596
Ivan Shybut <ivanshybut@192.168.100.5>
2021-12-10 21:29:58 +0300

    import

---
+++ test_start.py
@@ -0,0 +1,7 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as chrome_options
+from selenium.webdriver.support.event_firing_webdriver import EventFiringWebDriver
+
+from abstract.selenium_listener import MyListener
+
